cd0ad2c3b175b336f91db026bff96ab74c50c289
JM <jean.colib.mfc@gmail.com>
2025-12-11 11:15:31 +0100

    avancement

---
+++ utils/utils.lisp
@@ -0,0 +1,73 @@
+(defun get-prop (mv prop)
+  (get mv prop)
+)
+
+(defun set-prop (mv prop val)
+  (setf (get mv prop) val)
+)
+; TODO : gestion des étiquettes
+(defun get-mem (vm cible) ;; 1. Parenthèses autour des arguments
+  (cond 
+    ((integerp cible) 
+      (aref (get-prop vm :mem) cible)) 
+    (t 
+     (get-prop vm cible))
+  )
+)
+
+(defun set-mem (vm registre valeur) ; A tester
+  (set-prop vm registre valeur))
+
+
+(defun vm_jmp_cond_helper (vm val_flt val_feq val_fgt label)
+  (let ((flt (get-mem vm :FLT))
+        (feq (get-mem vm :FEQ))
+        (fgt (get-mem vm :FGT)))
+    (cond 
+      ((or (= flt val_flt) (= feq val_feq) (= fgt val_fgt)) 
+        (vm_exec_inst_JMP vm label))
+    ))
+)
+
+; Prend une valeur ou un registre, retourne la valeur (:CONST 4 -> 4, :REF R4 -> valeur stockée dans le registre R4)
+(defun read_value (vm val)
+  (cond
+    ((integerp val)
+      ;; 1. Si c'est un entier -> Adresse mémoire
+     (get-mem vm val))
+
+    ((listp val)
+      ;; 2. Si c'est une liste -> On regarde le premier élément
+     (case (first val)
+       (:CONST (second val))             ; Valeur brute
+       (:REF   (get-mem vm (second val))) ; Soit un registre soit une étiquette
+       (t (error "Type d'opérande inconnu : ~S" val))))
+
+    (t (error "Argument invalide (ni entier ni liste) : ~S" val))))
+
+(defun afficher_registres (vm)
+  (format t "~%=== État de la VM : ~S ===~%" vm)
+  
+  ;; Registres Généraux
+  (format t " [Registres] R0: ~4D | R1: ~4D | R2: ~4D | R3: ~4D~%"
+          (get-prop vm :R0)
+          (get-prop vm :R1)
+          (get-prop vm :R2)
+          (get-prop vm :R3))
+  
+  ;; Pile et Pointeurs
+  (format t " [Piles/PC]  PC: ~4D | SP: ~4D | BP: ~4D | FP: ~4D | LC: ~4D~%"
+          (get-prop vm :PC)
+          (get-prop vm :SP)
+          (get-prop vm :BP)
+          (get-prop vm :FP)
+          (get-prop vm :LC))
+  
+  ;; Comparaisons (Flags)
+  (format t " [Drapeaux]  FLT: ~A | FEQ: ~A | FGT: ~A~%"
+          (get-prop vm :FLT)
+          (get-prop vm :FEQ)
+          (get-prop vm :FGT))
+          
+  (format t "=============================~%")
+  (values)) ;; Retourne rien de spécifique pour ne pas polluer l'affichage
--- vm.lisp
+++ vm.lisp
@@ -1,22 +1,4 @@
-; par convention, résultat des fonctions stocké dans R0
-
-(PUSH <n>)
-(MOVE FP R1)
-(MOVE SP FP)
-(MOVE SP R2)
-(SUB @u? R2)
-(DECR R2)
-(PUSH R2)
-(PUSH R1)
-(JUMP )
-
-(defun get-prop (mv prop)
-  (get mv prop)
-)
-
-(defun set-prop (mv prop val)
-  (setf (get mv prop) val)
-)
+(require "utils/utils.lisp")
 
 ;Structure Mémoire : avec le code=Array et la pile=Array et stackpointer basepointer ? 
 (defun make_vm (name size)
@@ -41,19 +23,19 @@
     (set-prop name :PC 0); Program counter, compteur ordinal
     (set-prop name :LC 0); Load counter, ????
 
-
     ;etiq Table de hashage pour les étiquettes
     ;etiqNr Table non résolue des étiquettes
 
     (set-prop name :mem (make-array size :initial-element 0)); Mémoire avec code + pile
+
+    'ok ;Retourne la string "ok"
 )
 
 ; 
 (defun vm_exec (vm) )
 ; 
-(defun vm_load_exec (vm) )
+(defun vm_load_exec (vm) ) ; charge et exécute du code
 ; 
-(defun load (vm) )
 
 ;;;;;
 
@@ -61,23 +43,70 @@
 (defun vm_set_registre (vm) )
 
 ; Manipulation mémoire/registres
-(defun vm_exec_inst_LOAD (src dest) )
+(defun vm_exec_inst_LOAD (vm src dest) 
+  ;(set-mem vm dest (read_value vm src))
+  (let (addr (
+    (cond
+      ((integerp src)
+        (src)
+      )
+      ((listp src)
+        (case (first src)
+          (:CONST (error "Pas de constante acceptée par load"))             ; erreur
+          (:REF   (get-mem vm (second src))) ; est un registre on recup sa valeur, on ira chercher à cette adresse
+          (+    
+            (let ((val-registre (get-mem vm (second src))) 
+                      (offset (third src)))                     
+                  (+ val-registre offset)))                 
+          ) 
+          (t (error "Type d'opérande inconnu : ~S" src))
+        ))
+        (t (error "Argument invalide (ni entier ni liste) : ~S")
+      )
+    )
+  ))
+  (set-mem vm dest (read_value vm sr)) 
+)
+
 (defun vm_exec_inst_STORE (src dest) )
 
 (defun vm_exec_inst_MOVE (vm src dest) 
-   (set-prop dest (get-prop(src)))
+  (set-mem vm dest (read_value vm sr))
 )
 
-(defun vm_exec_inst_ADD (vm src dest) )
-(defun vm_exec_inst_SUB (vm src dest) )
-(defun vm_exec_inst_MUL (vm src dest) )
-(defun vm_exec_inst_DIV (vm src dest) )
-(defun vm_exec_inst_INCR (vm dest) )
-(defun vm_exec_inst_DECR (vm dest) )
+(defun vm_exec_inst_ADD (vm src dest) ; DEST += SRC
+  (set-mem vm dest (+ (read_value vm src) (read_value vm dest)))
+)
+
+(defun vm_exec_inst_SUB (vm src dest) ; DEST -= SRC
+  (set-mem vm dest (+ (read_value vm src) (read_value vm dest)))
+)
+(defun vm_exec_inst_MUL (vm src dest) ; DEST *= SRC
+  (set-mem vm dest (* (read_value vm src) (read_value vm dest)))
+)
+
+(defun vm_exec_inst_DIV (vm src dest) ; DEST /= SRC
+  (set-mem vm dest (/ (read_value vm dest) (read_value vm src)))
+)
+
+(defun vm_exec_inst_INCR (vm dest) ; DEST += 1
+  (set-mem vm dest (+ (read_value vm dest) 1))
+)
+
+(defun vm_exec_inst_DECR (vm dest) ; DEST -= 1
+  (set-mem vm dest (- (read_value vm dest) 1))
+)
 
 ; Pile
-(defun vm_exec_inst_PUSH (vm src) )
-(defun vm_exec_inst_POP (vm dest) )
+(defun vm_exec_inst_PUSH (vm src) ;push src sur la pile et incrémente SP
+  (vm_exec_inst_incr(vm :REF SP)) ; incrémente SP
+  (vm_exec_inst_move(vm src :REF SP))
+); On incrémente puis on met la valeur
+
+(defun vm_exec_inst_POP (vm dest) ;pop est met dans dest la valeur au sommet de la pile et décrémente SP
+  (vm_exec_inst_move(vm :REF SP dest))
+  (vm_exec_inst_decr(vm :REF SP)) ; décrémente SP
+); On enlève la valeur puis on décrémente
 
 ; Labels/sauts 
 (defun vm_exec_inst_LABEL (vm label) )
@@ -88,26 +117,46 @@
 (defun vm_exec_inst_RTN (vm) )
 
 ; Comparaisons
-(defun vm_exec_inst_CMP (vm src1 src2) )
+(defun vm_exec_inst_CMP (vm src1 src2)
 ; alloue respectivement les registres FLT, FEQ, FGT à 1 pour celui qui est vrai, 0 pour les autres. Par ex si src1 > src2, on a 001
-(
-    (set-prop (vm FLT 0))
-    (set-prop (vm FLT 0))
-    (set-prop (vm FLT 0))
+  (let 
+    ((val1 (get-mem vm src1)) 
+    (val2 (get-mem vm src2)))
 
-    if (src1 < src2) ( set-prop (vm FLT 1) ) 
-    if (src1 = src2) ( set-prop (vm FEQ 1) ) 
-    if (src1 > src2) ( set-prop (vm FGT 1) ) 
-    
+    (set-mem vm :FLT 0)
+    (set-mem vm :FEQ 0)
+    (set-mem vm :FGT 0)
+
+    (cond
+      ((< val1 val2) (set-mem vm :FLT 1))
+      ((= val1 val2) (set-mem vm :FEQ 1))
+      ((> val1 val2) (set-mem vm :FGT 1)))
+  )
 )
-(defun vm_exec_inst_JGT (vm label) )
-(defun vm_exec_inst_JGE (vm label) )
 
-(defun vm_exec_inst_JLT (vm label) )
-(defun vm_exec_inst_JLE (vm label) )
+(defun vm_exec_inst_JGT (vm label)
+  (vm_jmp_cond_helper vm 0 0 1 label)
+)
 
-(defun vm_exec_inst_JEQ (vm label) )
-(defun vm_exec_inst_JNE (vm label) )
+(defun vm_exec_inst_JGE (vm label) 
+  (vm_jmp_cond_helper vm 0 1 1 label)
+)
+
+(defun vm_exec_inst_JLT (vm label) 
+  (vm_jmp_cond_helper vm 1 0 0 label)
+)
+
+(defun vm_exec_inst_JLE (vm label) 
+  (vm_jmp_cond_helper vm 1 1 0 label)
+)
+
+(defun vm_exec_inst_JEQ (vm label) 
+  (vm_jmp_cond_helper vm 0 1 0 label)
+)
+
+(defun vm_exec_inst_JNE (vm label) 
+  (vm_jmp_cond_helper vm 1 0 1 label)
+)
 
 ; Tests logiques
 (defun vm_exec_inst_TEST  (vm src) )
@@ -117,7 +166,3 @@
 ; Contrôle neutre
 (defun vm_exec_inst_NOP (vm) )
 (defun vm_exec_inst_HALT (vm) )
-
-
-
-
