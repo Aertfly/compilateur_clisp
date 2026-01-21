313bf923c23cc7f8f709516a477d588682a53f28
Pierre Prevost--Avenel <pierre.prevost-avenel@etu.umontpellier.fr>
2026-01-21 16:10:52 +0100

    Gestion des comparaisons

--- compilateur.lisp
+++ compilateur.lisp
@@ -5,24 +5,57 @@
         (reste (cdddr exp)))
     (if (null reste)
         ; cas 2 arguments
-        (append (compilation arg1)
-                '((PUSH :R0))
-                (compilation arg2)
-                '((POP :R1))
-                (case op
-                  (+ '((ADD :R1 :R0)))
-                  (- '((SUB :R0 :R1) (MOVE :R1 :R0)))
-                  (* '((MUL :R1 :R0)))
-                  (/ '((DIV :R0 :R1) (MOVE :R1 :R0)))))
+        (append 
+          (compilation arg1)
+          '((PUSH :R0))
+          (compilation arg2)
+          '((POP :R1))
+          (case op
+            (+ '((ADD :R1 :R0)))
+            (- '((SUB :R0 :R1) (MOVE :R1 :R0)))
+            (* '((MUL :R1 :R0)))
+            (/ '((DIV :R0 :R1) (MOVE :R1 :R0)))))
         ; cas >2 arguments
         (compilation `(,op (,op ,arg1 ,arg2) ,@reste)))))
 
+(defun compilation-comp (exp)
+  (let* ((op (car exp))
+         (args (cdr exp))
+         (label-false (gensym "FALSE"))
+         (label-end (gensym "END"))
+         (code '()))
+    
+    (setf code (append code (compilation (car args))))
+    
+    (dolist (next-arg (cdr args))
+      (setf code (append code 
+        '((PUSH :R0))
+        (compilation next-arg)
+        '((POP :R1))
+        '((CMP :R1 :R0))
+        (case op
+          (<  `((JGE ,label-false)))
+          (>  `((JLE ,label-false)))
+          (=  `((JNE ,label-false)))
+          (<= `((JGT ,label-false)))
+          (>= `((JLT ,label-false)))
+        ))))
+
+    (setf code (append code
+      '((MOVE (:CONST T) :R0))
+      `((JMP ,label-end))
+      `((LABEL ,label-false))
+      '((MOVE (:CONST NIL) :R0))
+      `((LABEL ,label-end))))
+    code))
+
 (defun compilation (exp)
   (cond
-    ((numberp exp) `((MOVE (:CONST ,exp) :R0))) ; Si c'est un nombre, on utilise :CONST
-    ((atom exp) `((MOVE ,exp :R0))) ; Si c'est un symbole, on le met tel quel (registre)
-    ((member (car exp) '(+ - * /)) (compilation-op exp))
-    ; Pour continuer : passer le reste (donc tout sauf op) à des fonctions dédiées
+    ((numberp exp) `((MOVE (:CONST ,exp) :R0)))
+    ((atom exp) `((MOVE ,exp :R0)))
+    ((member (car exp) '(+ - * /)) (compilation-op exp)) ; Opération arithmétiques
+    ((member (car exp) '(< > = <= >=)) (compilation-comp exp)) ; Comparaison
     (t (error "Opérateur inconnu"))))
 
-(compilation '(+ 1 2 3 4))
+(defun compiler-expression (exp)
+  (append (compilation exp) '((HALT))))
--- test.lisp
+++ test.lisp
@@ -1,30 +1,13 @@
 (require "vm.lisp")
-(load "compilateur.lisp") ; On charge ton compilateur
+(load "compilateur.lisp")
 
 (defun test-compilation-run ()
-  (vm_make 'ultimate-vm 500)
-  
-  ;; L'expression Lisp que l'on veut tester
-  (let* ((expression '(+ 100 (- 75 25))) ; Résultat attendu : 150
-         ;; On appelle ta fonction compilation pour générer le code assembleur
-         (code (append (compilation expression) '((HALT)))))
+  (vm_make 'vm 500)
+  (vm_load (
+    compiler-expression '
+      (> 5 4 5 2 1)
+    ) 'vm)
+  (vm_run 'vm)    
+  (format t "~%~S~%" (get-prop 'vm :R0)))
 
-    (format t "~%--- TEST : COMPILATION ET EXÉCUTION ---~%")
-    (format t "Expression : ~A~%" expression)
-    (format t "Code généré : ~A~%~%" code)
-
-    ;; On charge le code généré dynamiquement dans la VM
-    (vm_load code 'ultimate-vm)
-    (vm_run 'ultimate-vm)
-    
-    (afficher_registres 'ultimate-vm)
-    
-    (let ((res (get-prop 'ultimate-vm :R0)))
-      (if (= res 150)
-          (format t "~%[SUCCÈS] R0 vaut bien 150. La compilation et l'exécution ont réussi.~%")
-          (format t "~%[ÉCHEC] R0 vaut ~D au lieu de 150.~%" res)))
-  )
-)
-
-;; Lancer le test
 (test-compilation-run)
